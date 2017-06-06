eed2b69b785243ef56c61eb6431da981b7500bdf
Shawna <shawnalehman@mail.com>
2017-06-05 21:48:21 -0400

    Ruby syntax and stuff…

--- filters.rb
+++ filters.rb
@@ -2,10 +2,38 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-
+  @candidates.select { |candidate| candidate[:id] === id}
 end
 
 def experienced?(candidate)
+  candidate[:years_of_experience] >= 2
 end
 
-# More methods will go below
+def github_points(candidate)
+  candidate[:github_points] >= 100
+end
+
+def languages(candidate)
+  candidate[:languages].include?("Ruby") || candidate[:languages].include?("Python")
+end
+
+def recent_appliction(candidate)
+  candidate[:date_applied] >= 15.days.ago.to_date
+end
+
+ def ofAge(candidate)
+  candidate[:age] >= 18
+end
+
+def qualified_applicant(candidates)
+  candidates.select do |candidate|
+  experienced?(candidate) && github_points(candidate) && languages(candidate) && recent_appliction(candidate) && ofAge(candidate)
+  end
+end
+
+
+def ordered_by_qualifications(candidates)
+  ordered_candidates = candidates.sort_by { |candidate| [candidate[:years_of_experience], candidate[:github_points]] }
+  return (ordered_candidates).reverse
+end
+
--- main.rb
+++ main.rb
@@ -1,3 +1,6 @@
+# This is the main entrypoint into the program
+# It requires the other files/gems that it needs
+
 require 'pry'
 require './candidates'
 require './filters'
@@ -6,4 +9,20 @@ require './filters'
 
 # binding.pry
 
-pp qualified_candidates
+# pp @candidates
+
+ # pp experienced?(@candidates)
+
+# pp find(5)
+
+# pp github_points(100)
+
+ # pp languages(@candidates)
+
+ # pp recent_appliction(@candidates)
+
+ # pp ofAge(@candidates)
+
+  # pp qualified_applicant(@candidates)
+
+pp ordered_by_qualifications(@candidates)
